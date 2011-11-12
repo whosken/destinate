f4adcf3d71cd3b195d72f8c9224970f4d75c2e16
Ken Hu <whosbacon@live.com>
2011-11-12 16:21:02 +0000

    document map reduced used for free-text search

---
+++ mapreduce/view_by_keyword.js
@@ -0,0 +1,18 @@
+var map = 
+function(doc) {
+  if (doc.profile)
+    for (var word in doc.profile)
+      emit(word, [doc._id]);
+};
+
+var reduce = 
+function(keys,values,rereduce) {
+  var result = values[0];
+  for (var i in values)
+    var value = values[i];
+    for (var j in value)
+      var name = value[j];
+      if (result.indexOf(name) < 0)
+        result.push(name);
+  return result;
+};
