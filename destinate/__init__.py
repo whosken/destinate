f7fc62d59a0802f18aaaa2e2f30a007835345275
Ken Hu <whosbacon@gmail.com>
2016-05-19 21:14:55 +0800

    fix region filter

--- destinate/__init__.py
+++ destinate/__init__.py
@@ -24,31 +24,28 @@ def build_name_query(city_name):
 def build_query_from_guide(guide, regions=None):
     summary = nlp.summarize(guide, max_sentence_count=30)
     query = {
-        'filtered':{
-            'query':{
+        'bool':{
+            'must':[{
                 'match':{
                     'guide':{
                         'query':summary,
                         'cutoff_frequency':0.005
                         }
                     }
-                },
-            'filter':{
-                'match_all':{}
                 }
-            }
+            ]}
         }
-    if regions: # TODO: fix
-        query['filtered']['filter'].pop('match_all',0)
+    if regions:
         if isinstance(regions, (unicode,str)):
             regions = [regions]
-        query['filtered']['filter'].update({
-            'terms':{
-                'regions':regions,
-                'execution':'or'
+        query['bool']['must'].append({
+            'match':{
+                'regions':{
+                    'query':regions,
+                    'operator':'or'
+                    }
                 }
             })
-    print query['filtered']['filter']
     return query
 
 def suggest_from_collaborative_filter(user):
