9c339ed5c1e0e85b821fd6c7b5506ce6392ec094
Ken Hu <whosbacon@gmail.com>
2016-05-27 11:02:18 -0700

    fix query summary

--- destinate/__init__.py
+++ destinate/__init__.py
@@ -5,10 +5,11 @@ import nlp
 def suggest_from_city(city_name, **options):
     seed = storage.search_cities(
         query_builder.build_from_name(city_name),
-        fields=['summary','cid','name']).next()
+        fields=['guide','cid','name']).next()
     print 'found', seed['name'].encode('utf8'), seed['cid']
     return suggest_from_summary(nlp.summarize(seed['guide']), seed['cid'])
-    
-def suggest_from_summary(summary, ignore_cid=None):
+
+def suggest_from_summary(summary, ignore_cid=None, **options):
     query = query_builder.build_from_summary(summary, **options)
-    return [c for c in storage.search_cities(query) if not ignore_cid or c['cid'] != ignore_cid]
+    valid_cid = lambda c: not ignore_cid or c['cid'] != ignore_cid
+    return filter(valid_cid, storage.search_cities(query))
