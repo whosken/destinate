bd2cc376d53bcde4c386a2b6ea5d1e95b4ef6a84
Ken Hu <whosbacon@gmail.com>
2016-05-29 13:08:29 -0700

    use must_not to filter out city by id or name

--- destinate/__init__.py
+++ destinate/__init__.py
@@ -9,12 +9,11 @@ def suggest_from_city(city_name, **options):
     print 'found', seed['name'].encode('utf8'), seed['cid']
     return suggest_from_summary(nlp.summarize(
         seed['guide']),
-        ignore_cid=seed['cid'],
         weather=seed['weather'],
+        ignore_cid=seed['cid'],
         **options
         )
 
-def suggest_from_guide(guide, ignore_cid=None, **options):
+def suggest_from_guide(guide, **options):
     query = query_builder.build_from_guide(guide, **options)
-    valid_cid = lambda c: not ignore_cid or c['cid'] != ignore_cid
-    return filter(valid_cid, storage.search_cities(query))
+    return storage.search_cities(query)
--- destinate/query_builder.py
+++ destinate/query_builder.py
@@ -9,7 +9,7 @@ def build_from_name(city_name):
             }
         }
     
-def build_from_guide(guide, regions=None, weather=None, months=None):
+def build_from_guide(guide, regions=None, weather=None, months=None, ignore_cid=None, ignore_name=None):
     must = [{
             'match':{
                 'guide':{
@@ -20,6 +20,7 @@ def build_from_guide(guide, regions=None, weather=None, months=None):
             }
         ]
     should = []
+    must_not = []
     if regions:
         if isinstance(regions, (unicode,str)):
             regions = [regions]
@@ -35,8 +36,15 @@ def build_from_guide(guide, regions=None, weather=None, months=None):
         should += [{'term':{'weather.{}'.format(k):v}} for k,v in weather.items()]
     if months:
         should.append({'terms':{'months_ideal':months}})
+    if ignore_cid:
+        must_not.append({'term':{'cid':ignore_cid}})
+    if ignore_name:
+        must_not.append(build_from_name(ignore_name))
+        
     query = {'bool':{'must':must}}
     if should:
         query['bool']['should'] = should
+    if must_not:
+        query['bool']['must_not'] = must_not
     return query
     
