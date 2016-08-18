4c74e1feb31703cc8fa039b13f7aac4c4f1f4c4d
Ken Hu <whosbacon@gmail.com>
2016-08-18 12:37:45 +0100

    enable jsonp requests

--- web.py
+++ web.py
@@ -20,6 +20,15 @@ def auth(func):
     
 def authenticate(token):
     return destinate.profile.is_valid_token(token)
+    
+def enable_jsonp_response(*args, **kwargs):
+    callback = flask.request.args.get('callback')
+    json_str = flask.json.dumps(*args, **kwargs)
+    if callback:
+        json_str = '{}({});'.format(callback, json_str)
+    response = flask.make_response(json_str)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/logins/',methods=['POST'])
 def login():
@@ -28,7 +37,7 @@ def login():
         user = destinate.profile.find_and_analyze_user(token)
     except ValueError:
         return flask.abort(400)
-    response = flask.make_response(flask.jsonify(user=user))
+    response = enable_jsonp_response(user=user)
     flask.session[SESSION_TOKEN] = token
     return response
 
@@ -44,5 +53,5 @@ def suggest():
         guide,
         months=months,
         ignore_name=user.get('city'))
-    return flask.jsonify(suggestions=cities)
+    return enable_jsonp_response(suggestions=cities)
     
